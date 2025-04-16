b5cec289f6709d1a3a89b726fd58c4d3e31dfb18
awel-stack <adolfowelch73@gmail.com>
2025-04-16 13:43:36 -0600

    error
    error

--- dashboard.py
+++ dashboard.py
@@ -22,7 +22,8 @@ def cargar_datos():
     try:
         registros = sheet.get_all_records()
         df = pd.DataFrame(registros)
-        df.columns = df.columns.astype(str).str.strip()  # Limpiar encabezados
+        df.columns = df.columns.astype(str).str.strip() # Limpiar encabezados
+        print("Columnas recibidas:", df.columns.tolist())  
         df['fecha'] = pd.to_datetime(df['fecha'])
         return df
     except Exception as e:
